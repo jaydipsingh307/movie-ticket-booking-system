c815c77b5de8aac7a9b1232356c8d71e88178ef0
Jaydeep Singh Chauhan <jaydeepsinghchauhan@Jaydeeps-MacBook-Air.local>
2026-07-02 17:48:59 +0530

    Fix middleware path

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const posters = require("../middleware/posters");
+const posters = require("../Middleware/posters");
 
 const {
   getMovies,
