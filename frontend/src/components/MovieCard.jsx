e4fec1948812032a85dd1b037ecf97a0251d27dd
Jaydeep Singh Chauhan <jaydeepsinghchauhan@Jaydeeps-MacBook-Air.local>
2026-07-05 13:32:50 +0530

    Move posters

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,44 +1,43 @@
 import { Link } from "react-router-dom";
 
 function MovieCard({ movie }) {
-    console.log(movie);
-    return (
-        <div className="col-md-4 mb-4">
-            <div className="card shadow h-100">
+  return (
+    <div className="col-md-4 mb-4">
+      <div className="card shadow-lg h-100">
 
-                <img
-                    src={`https://movie-ticket-backend-5t6p.onrender.com/${movie.image}`}
-                    className="card-img-top"
-                    alt={movie.title}
-                    style={{
-                        height: "400px",
-                        objectFit: "cover"
-                    }}
-                />
-                <div className="card-body">
-                    <h5>{movie.title}</h5>
+        <img
+          src={`https://movie-ticket-backend-5t6p.onrender.com${movie.image}`}
+          alt={movie.title}
+          className="card-img-top"
+          style={{
+            height: "400px",
+            objectFit: "cover",
+          }}
+        />
 
-                    <p><b>Genre:</b> {movie.genre}</p>
+        <div className="card-body">
+          <h5 className="card-title">{movie.title}</h5>
 
-                    <p><b>Language:</b> {movie.language}</p>
+          <p><b>Genre:</b> {movie.genre}</p>
 
-                    <p><b>Duration:</b> {movie.duration}</p>
+          <p><b>Language:</b> {movie.language}</p>
 
-                    <h5 className="text-success">₹ {movie.price}</h5>
+          <p><b>Duration:</b> {movie.duration}</p>
 
+          <h5 className="text-success">₹{movie.price}</h5>
 
-                    <Link
-                        to="/booking"
-                        state={{ movie }}
-                        className="btn btn-danger w-100"
-                    >
-                        Book Ticket
-                    </Link>
+          <Link
+            to="/booking"
+            state={{ movie }}
+            className="btn btn-danger w-100"
+          >
+            Book Ticket
+          </Link>
 
-                </div>
-            </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default MovieCard;
