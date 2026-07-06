36323345568582fe65c64690a6f5c23dde347275
Jaydeep Singh Chauhan <jaydeepsinghchauhan@Jaydeeps-MacBook-Air.local>
2026-07-06 11:26:03 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import Movies from "./Pages/Movie";
-import Booking from "./Pages/Booking";
-import Success from "./Pages/Success";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import Payment from "./Pages/Payment";
-import TicketHistory from "./Pages/TicketHistory";
-import Admin from "./Pages/Admin";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/history" element={<TicketHistory />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <h1 style={{ textAlign: "center", marginTop: "100px" }}>
+        Website Working ✅
+      </h1>
     </BrowserRouter>
   );
 }
